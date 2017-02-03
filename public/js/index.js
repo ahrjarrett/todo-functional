3f4f6fe9391e5a8e9d40f2115de60353a2d6d19f
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-03 16:49:50 -0700

    added (ugly) type functionality; next, filter

--- public/js/index.js
+++ public/js/index.js
@@ -15,16 +15,28 @@ const deleteTodo = (el) => {
 const saveTodo = document.getElementById('save-todo')
 const todos = document.getElementById('todos')
 const newTodo = document.getElementById('new-todo')
+const newType = document.getElementById('new-type')
+const createType = document.getElementById('create-type')
+const saveType = document.getElementById('save-type')
+const selectType = document.getElementById('select-type')
 
 let counter = 0
 
 saveTodo.addEventListener('click', (e) => {
   e.preventDefault()
   const todoValue = newTodo.value || ''
+  const type = selectType.value
+  console.log(type)
 
   const todo = document.createElement('div')
   const todoText = todo.appendChild(document.createElement('p'))
   todoText.innerHTML = todoValue
+  newTodo.value = ''
+
+  const typeInfo = todo.appendChild(document.createElement('a'))
+  typeInfo.innerHTML = `[type: ${type}]`
+  typeInfo.classList.add('type-info', `type-${type}`)
+  typeInfo.href = '#'
 
   const deleteBtn = todo.appendChild(document.createElement('a'))
   todo.classList.add('todo')
@@ -32,16 +44,36 @@ saveTodo.addEventListener('click', (e) => {
   deleteBtn.classList.add('delete-todo')
   deleteBtn.href = '#'
 
-  // this is brittle, using counter then not incrementing it. rm side-effects
+
   assignId(deleteBtn, counter)
   assignId(todo, counter++)
-
   todos.appendChild(todo)
 
   deleteBtn.addEventListener('click', (e) => {
     e.preventDefault()
     deleteTodo(e.target)
   })
+})
+
+createType.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  newType.classList.add('show-item')
+  saveType.classList.add('show-item')
+})
+
+saveType.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const typeName = newType.value
+  const type = document.createElement('option')
+  type.value, type.text = typeName
+  if(typeName) selectType.appendChild(type)
+  newType.value = ''
+
+
+  newType.classList.remove('show-item')
+  saveType.classList.remove('show-item')
 
 })
 
